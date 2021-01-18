7466b3fbd35aae2365ed0d2223a692678ce3e17d
Dustin Spicuzza <dustin@virtualroadside.com>
2021-01-18 00:46:45 -0500

    Improve cache server
    - It no longer cleans up the SSH connection
    - Mechanism added to map arbitary files onto the cache
    - Logging goes through the logger now

--- robotpy_installer/cacheserver.py
+++ robotpy_installer/cacheserver.py
@@ -1,26 +1,54 @@
 import logging
+import pathlib
 import threading
 from http.server import SimpleHTTPRequestHandler
 
-logger = logging.getLogger("robotpy.installer")
+from robotpy_installer.sshcontroller import SshController
+
+logger = logging.getLogger("cacheserver")
+
+
+class HTTPHandler(SimpleHTTPRequestHandler):
+    def __init__(self, mapped_files, *args, **kwargs):
+        self.mapped_files = mapped_files
+        super().__init__(*args, **kwargs)
+
+    def log_message(self, format: str, *args) -> None:
+        logger.debug(f"%s {format}", self.address_string(), *args)
+
+    def translate_path(self, path):
+        xpath = path.split("?", 1)[0]
+        xpath = xpath.split("#", 1)[0]
+        redirect = self.mapped_files.get(xpath)
+        if redirect:
+            return redirect
+
+        return super().translate_path(path)
 
 
 class CacheServer:
-    def __init__(self, ssh_controller, cache_root):
+    def __init__(self, ssh_controller: SshController, cache_root: pathlib.Path):
         self.controller = ssh_controller
         self.cache_root = cache_root
 
-        self.controller.ssh_connect()
         self.transport = self.controller.client.get_transport()
-        self.pipe_port = self.transport.request_port_forward("", 0)
+        self.port = self.transport.request_port_forward("", 0)
 
-    def close(self):
-        self.controller.ssh_close_connection()
+        self.mapped_files = {}
+
+    def add_mapping(self, fname: str, local_file: pathlib.Path):
+        self.mapped_files[fname] = local_file
+
+    def start(self):
+        t = threading.Thread(target=self._handle_requests)
+        t.setDaemon(True)
+        t.start()
 
     def process_request(self, request):
         client_address = request.getpeername()
         try:
-            SimpleHTTPRequestHandler(
+            HTTPHandler(
+                self.mapped_files,
                 request=request,
                 client_address=client_address,
                 server=None,
@@ -32,11 +60,12 @@ class CacheServer:
         finally:
             request.close()
 
-    def handle_requests(self):
+    def _handle_requests(self):
         request = self.transport.accept()
 
         while request is not None:
             t = threading.Thread(target=self.process_request, args=[request])
             t.setDaemon(True)
-            t.run()
+            t.start()
+
             request = self.transport.accept()
